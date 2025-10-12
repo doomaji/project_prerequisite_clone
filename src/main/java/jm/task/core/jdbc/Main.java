a27c78f46eb4bc889a5e1159108a2e27772439fd
doomaji <doomaji@gmail.com>
2025-10-13 01:00:04 +0900

    4. Try to implicate Hibernate.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,6 +3,8 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.*;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
 
 import java.sql.*;
 import java.util.*;
@@ -15,20 +17,33 @@ public class Main {
         userService.createUsersTable();
 
         User firstUser = new User("Curtis", "Jackson", (byte) 50);
-        User secondUser = new User("Marshall", "Mathers", (byte) 52);
-        User thirdUser = new User("Fergie", "Duhamel", (byte) 50);
-        User fourthUser = new User("Ashley", "Frangipanie", (byte) 30);
+//        User secondUser = new User("Marshall", "Mathers", (byte) 52);
+//        User thirdUser = new User("Fergie", "Duhamel", (byte) 50);
+//        User fourthUser = new User("Ashley", "Frangipanie", (byte) 30);
+//
+//        userService.saveUser(firstUser.getName(), firstUser.getLastName(), firstUser.getAge());
+//        userService.saveUser(secondUser.getName(), secondUser.getLastName(), secondUser.getAge());
+//        userService.saveUser(thirdUser.getName(), thirdUser.getLastName(), thirdUser.getAge());
+//        userService.saveUser(fourthUser.getName(), fourthUser.getLastName(), fourthUser.getAge());
+//
+//        System.out.println(userService.getAllUsers());
+//
+//        userService.cleanUsersTable();
+//        System.out.println(userService.getAllUsers());
+//        userService.dropUsersTable();
 
-        userService.saveUser(firstUser.getName(), firstUser.getLastName(), firstUser.getAge());
-        userService.saveUser(secondUser.getName(), secondUser.getLastName(), secondUser.getAge());
-        userService.saveUser(thirdUser.getName(), thirdUser.getLastName(), thirdUser.getAge());
-        userService.saveUser(fourthUser.getName(), fourthUser.getLastName(), fourthUser.getAge());
+        //Get Session
+        Session session = Util.HibernateUtil.getSessionFactory().getCurrentSession();
+        //start transaction
+        session.beginTransaction();
+        //Save the Model object
+        session.save(firstUser);
+        //Commit transaction
+        session.getTransaction().commit();
+        System.out.println("User ID="+firstUser.getId());
 
-        System.out.println(userService.getAllUsers());
-
-        userService.cleanUsersTable();
-        System.out.println(userService.getAllUsers());
-        userService.dropUsersTable();
+        //terminate session factory, otherwise program won't end
+        Util.HibernateUtil.getSessionFactory().close();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,6 +1,11 @@
 package jm.task.core.jdbc.util;
 
 import java.sql.*;
+import java.util.Properties;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/example_schema";
@@ -16,5 +21,36 @@ public class Util {
             System.err.println(e.getMessage());
         }
         return connection;
+    }
+
+    public class HibernateUtil {
+
+        private static SessionFactory sessionFactory;
+
+        private static SessionFactory buildSessionFactory() {
+            try {
+                // Create the SessionFactory from hibernate.cfg.xml
+                Configuration configuration = new Configuration();
+                configuration.configure("hibernate.cfg.xml");
+                System.out.println("Hibernate Configuration loaded");
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+                System.out.println("Hibernate serviceRegistry created");
+
+                SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+                return sessionFactory;
+            }
+            catch (Throwable ex) {
+                // Make sure you log the exception, as it might be swallowed
+                System.err.println("Initial SessionFactory creation failed." + ex);
+                throw new ExceptionInInitializerError(ex);
+            }
+        }
+
+        public static SessionFactory getSessionFactory() {
+            if (sessionFactory == null) sessionFactory = buildSessionFactory();
+            return sessionFactory;
+        }
     }
 }
